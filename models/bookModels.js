24103ee2916bf12ae7f26caa8effc54bc10ec8a6
ramsumble <ram.sumble@gmail.com>
2024-02-05 21:28:24 +1100

    created the book model for mongo, fixed weird spacing in userModels

---
+++ models/bookModels.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const bookSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    author: { type: String },
+    publisher: { type: String },
+    description: { type: String },
+    yearPublished: { type: Number },
+    genre: { type: String },
+    pageCount: {type: Number }
+});
+
+const BookModel = mongoose.model('Book', bookSchema);
+
+module.exports = BookModel;
