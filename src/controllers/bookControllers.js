b62e6e01570d48cbff69543c3304d9b6266cb977
ramsumble <132447922+ramsumble@users.noreply.github.com>
2024-03-04 11:05:43 +1100

    rollback for bookcontroller.js

--- src/controllers/bookControllers.js
+++ src/controllers/bookControllers.js
@@ -1,55 +1,39 @@
 const axios = require('axios');
 const BookModel = require('../models/bookModels');
-const UserModel = require('../models/userModels');
+// const UserModel = require('../models/userModels');
 
 // search for books based on the search term
-async function searchAndSaveBooks(userId, searchTerm) {
+async function searchAndSaveBooks(searchTerm) {
     try {
         const response = await axios.get('https://www.googleapis.com/books/v1/volumes', { 
             params: { q: searchTerm },
     });
 
-    // get the book data from the response 
-    // const bookDataFromApi = response.data.items.map(item => item.volumeInfo);
+  // get the book data from the response 
+  // const bookDataFromApi = response.data.items.map(item => item.volumeInfo);
     const bookDataFromApi = response.data.items.map(item => {
-      const volumeInfo = item.volumeInfo;
-      return {
-          title: volumeInfo.title,
-          author: volumeInfo.authors ? volumeInfo.authors.join(', ') : undefined,
-          publisher: volumeInfo.publisher,
-          description: volumeInfo.description,
-          yearPublished: volumeInfo.publishedDate ? new Date(volumeInfo.publishedDate).getFullYear() : undefined,
-          genre: volumeInfo.categories ? volumeInfo.categories.join(', ') : undefined,
-          pageCount: volumeInfo.pageCount,
-          image: volumeInfo.imageLinks ? volumeInfo.imageLinks.thumbnail : undefined
-      };
-  });
+        const volumeInfo = item.volumeInfo;
+        return {
+            title: volumeInfo.title,
+            author: volumeInfo.authors ? volumeInfo.authors.join(', ') : undefined,
+            publisher: volumeInfo.publisher,
+            description: volumeInfo.description,
+            yearPublished: volumeInfo.publishedDate ? new Date(volumeInfo.publishedDate).getFullYear() : undefined,
+            genre: volumeInfo.categories ? volumeInfo.categories.join(', ') : undefined,
+            pageCount: volumeInfo.pageCount,
+            image: volumeInfo.imageLinks ? volumeInfo.imageLinks.thumbnail : undefined
+        };
+});
 
     // const savedBooks = await BookModel.create(bookDataFromApi); //remove later when we store only selected books from front end
-  const savedBooks = await Promise.all(bookDataFromApi.map(async (bookData) => {
-    const existingBook = await BookModel.findOne({ title: bookData.title, author: bookData.author });
 
-    if (existingBook) {
-        // If the book already exists return the id
-        return existingBook._id;
-        
-    } else {
-        // create the book and return id
-        const newBook = await BookModel.create(bookData);
-        return newBook._id;
-    }
-  }));
+    // return savedBooks;
+    return bookDataFromApi;
 
-  // Update the users collection with the new book ids
-  await UserModel.findByIdAndUpdate(userId, { $push: { bookCollection: { $each: savedBooks } } });
-
-  return savedBooks;    
-  // return bookDataFromApi;
-
-  } catch (error) {
-    console.error('Error searching and saving books:', error);
-    throw error; // Propagate the error to the caller
-  }
+} catch (error) {
+  console.error('Error searching and saving books:', error);
+  throw error; // Propagate the error to the caller
+}
 }
 
 module.exports = { searchAndSaveBooks };
