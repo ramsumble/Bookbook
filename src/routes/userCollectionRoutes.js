7de45c227e4b1b98c7f629dbe9f7056c9dbafd9b
ramsumble <ram.sumble@gmail.com>
2024-03-05 22:07:34 +1100

    added logging for user collection route

--- src/routes/userCollectionRoutes.js
+++ src/routes/userCollectionRoutes.js
@@ -6,8 +6,11 @@ const { authenticateUser } = require('../controllers/authControllers')
   router.get('/', authenticateUser, async (req, res) => {
     try {
       const userId = req.userId; 
-  
       const userBookCollection = await getUserBookCollection(userId);
+
+      // logging to check contents of book collection 
+      console.log('User Book Collection:', userBookCollection);
+
       res.json(userBookCollection);
     } catch (error) {
       res.status(500).json({ error: 'Internal Server Error' });
