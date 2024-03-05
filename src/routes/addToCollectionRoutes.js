cd9e0e57082de6428ed369a8dffb6c6b6694129c
ramsumble <ram.sumble@gmail.com>
2024-03-05 12:56:44 +1100

    Update addToCollectionRoutes.js
    adding similar logic from the bookController to add books to user collection

--- src/routes/addToCollectionRoutes.js
+++ src/routes/addToCollectionRoutes.js
@@ -16,26 +16,35 @@ router.post('/', async (req, res) => {
         if (!user.bookCollection) {
             user.bookCollection = [];
         }
-        
-        // check if the book data is valid
-        // if (!bookData || !bookData._id) {
-        //     console.error('Invalid book data:', bookData);
-        //     return res.status(400).json({ error: 'Invalid book data' });
-        //   }
 
-        // Check if the book already exists 
+        // Check if the book data is valid
+        if (!bookData || !bookData.title || !bookData.author) {
+            console.error('Invalid book data:', bookData);
+            return res.status(400).json({ error: 'Invalid book data' });
+        }
+
+        // Check if the book already exists in the user's collection
         const existingBook = user.bookCollection.find(bookId => bookId.equals(bookData._id));
 
         if (existingBook) {
             return res.status(400).json({ error: 'Book already in the collection' });
         }
 
-        // Add the book to the users collection
-        user.bookCollection.push(bookData._id);
+        // Create or retrieve the book in the database
+        const existingBookInDB = await BookModel.findOne({ title: bookData.title, author: bookData.author });
+
+        if (existingBookInDB) {
+            // If the book already exists in the database, add its ID to the user's collection
+            user.bookCollection.push(existingBookInDB._id);
+        } else {
+            // If the book doesn't exist, create it and add its ID to the user's collection
+            const newBook = await BookModel.create(bookData);
+            user.bookCollection.push(newBook._id);
+        }
+
         await user.save();
 
         res.json({ message: 'Book added to collection successfully' });
-
     } catch (error) {
         console.error('Error adding book to collection:', error);
         res.status(500).json({ error: 'Internal Server Error' });
