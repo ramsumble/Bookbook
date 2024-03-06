89394ca8b2e294c4ca2739b493bc26c829d57680
ramsumble <ram.sumble@gmail.com>
2024-03-06 22:01:00 +1100

    added usermodel to include favourites

--- src/models/userModels.js
+++ src/models/userModels.js
@@ -6,6 +6,7 @@ const userSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     bookCollection: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Book' }],
+    favourites: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Book' }],
 });
 
 userSchema.pre('save', async function (next) {
