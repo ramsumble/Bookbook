6f183539d1a696baae20c64bb30407022de1167f
ramsumble <ram.sumble@gmail.com>
2024-03-08 23:05:17 +1100

    updating user schema to include IsRead field for BookCollection, updated post route for the new field

--- src/models/userModels.js
+++ src/models/userModels.js
@@ -3,14 +3,17 @@ const bcrypt = require('bcrypt');
 
 // explaination for schemas: 
 // bookCollection will store all of the users read books 
-//
-//
-//
+//  IsRead - user will mark a book as complete once they have finished reading it
+// Favourites is for the collection that users want their fav books to be in
+// reading collection is the books the user is currently reading 
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    bookCollection: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Book' }],
+    bookCollection: [{
+        bookId: { type: mongoose.Schema.Types.ObjectId, ref: 'Book', required: true },
+        isRead: { type: Boolean, default: false },
+    }],
     favourites: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Book' }],
     readingCollection: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Book' }],
 });
